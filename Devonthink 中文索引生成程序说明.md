bd4cb7c355b3e222be8d729b6726b469c22d4a02
ringsaturn <tltrc919@gmail.com>
2017-04-25 10:58:39 +0800

    发布

---
+++ Devonthink 中文索引生成程序说明.md
@@ -0,0 +1,26 @@
+# Devonthink 中文索引生成程序说明
+
+## 用法
+
+1. 复制笔记内容（有图片，英文，等都可以，反正会被过滤掉）
+2. 运行程序
+3. 在相应笔记的 Spotlight Comments 处粘贴剪贴板内容（可以通过 Command+Shift+I 打开）
+
+## 依赖
+
+1.python 3.x<br>
+2.[结巴分词](https://github.com/fxsjy/jieba): `pip3 install jieba` <br>
+3.[pyperclip](https://github.com/asweigart/pyperclip): `pip3 install pyperclip`<br>
+
+可选：根据个人需求而定，一般不用，详情见代码<br>
+[thulac](https://github.com/thunlp/THULAC-Python): `pip install thulac`
+
+
+## 函数说明
+
+代码中已有说明文字
+
+## 待完成
+
+1. 代码中的 TODO 部分
+2. 发布到 pip 中方便使用
---
+++ get_key_words.py
@@ -0,0 +1,78 @@
+#coding=utf-8
+#! /usr/local/bin/python3
+
+import string
+import jieba.analyse
+import pyperclip
+import re
+
+# 过滤
+def filter_chinese(text):
+	#r = re.sub("[A-Za-z0-9\[\`\~\!\@\#\$\^\&\*\(\)\=\|\{\}\'\:\;\'\,\[\]\.\<\>\/\?\~\\\！\@\#\\\&\*\%]", "", s)
+	r = re.sub("[A-Za-z0-9\[\`\~\!\@\#\$\^\&\*\(\)\=\|\{\}\'\:\：\。\，\“\“\_\-\”\▃\？\、\/\n\\\─\\;\'\,\[\]\.\<\>\/\?\~\\《\》\）\\\（\\！\@\#\\\&\*\%]", "", text)
+	r = r.replace(' ', '')
+	return r
+
+def copy_to_clipboard(keywords,method=','):
+	# 可以用 其他标点符号替换 替换
+	method = ",";
+	#print(str.join( seq ))
+	output_str = method.join(str(keywords[0]))
+	return output_str
+
+#TODO
+# 完善分词方式，可以自由使用 THULAC ／ 结巴分词，这个也要变成一个函数
+# 根据字符串长度先估计运行时间，再执行分词程序的初始化 2017-04-21 22:06:18
+
+# 这部分根据个人需求修改 2017-04-25 10:27:16
+# 0 → 增加一步清华分词
+# 其他 → 结巴分词直接提取关键词，没有区别
+def cut(content, method=0):
+	if method == 0:
+		import thulac
+		thu1 = thulac.thulac(seg_only=True,filt=True)
+		words = thu1.cut(content, text=True)  #进行一句话分词
+	else:
+		words = content
+	return words
+
+#TODO
+# 优化关键词过滤方式
+# 参数可更改
+def filter_keywords(keywords):
+	final_keywords = []
+	for item in keywords:
+		if item[1]>0.01:
+			final_keywords.append(item[0])
+	return final_keywords
+
+
+def get_key_words(content):
+
+	# 冲洗字符串，只留下汉字
+	content = filter_chinese(content) # 过滤
+
+	# 假设单词平均长度2 估计最多有多少词，
+	text_length = len(content)
+	words_num_max = int(text_length/2)
+
+	# 分词
+	words = cut(content)
+
+	# 获取关键词
+	keywords = jieba.analyse.extract_tags(words, topK=words_num_max, withWeight=True, allowPOS=( ))
+
+	# 对关键词进行筛选
+	final_keywords = filter_keywords(keywords)
+
+	# 去除无用的符号
+	final_keywords = str(final_keywords).replace('\'','')
+	final_keywords = str(final_keywords).replace('[','')
+	final_keywords = str(final_keywords).replace(']','')
+
+	# 拷贝到剪贴板
+	pyperclip.copy(final_keywords)
+
+if __name__ == '__main__':
+	content = str(pyperclip.paste())
+	get_key_words(content)
